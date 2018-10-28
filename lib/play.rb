3c092808c8d814dad351a5c6f7ca2570e99f47dc
Yasmin Torres <yasmintorres90@hotmail.com>
2018-10-28 23:11:57 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -23,7 +23,7 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board)
+def play(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
@@ -31,7 +31,7 @@ def turn(board)
     move(board, index)
     display_board(board)
   else
-    turn
+    turn(board)
   end
 end
 
